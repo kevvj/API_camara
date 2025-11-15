be8f7c690c4ab925fd58b727954718f26ffce229
kevvj <kevinjvillalba774+1@gmail.com>
2025-11-15 02:58:34 -0500

    cors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { createClient } = require('@supabase/supabase-js');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { createClient } = require('@supabase/supabase-js')
+const cors = require('cors')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+app.use(cors())
+const PORT = 3000
 
-const supabaseUrl = 'https://qukomivcjxiwtmecebnb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1a29taXZjanhpd3RtZWNlYm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNzEwOTQsImV4cCI6MjA3ODc0NzA5NH0.lIOvl8Fis8dwaGlQHvDy4qsZ1-W2NQJ_zTC0uHE_sRY';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://qukomivcjxiwtmecebnb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1a29taXZjanhpd3RtZWNlYm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNzEwOTQsImV4cCI6MjA3ODc0NzA5NH0.lIOvl8Fis8dwaGlQHvDy4qsZ1-W2NQJ_zTC0uHE_sRY'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.post('/api/test', (req, res) => {
   console.log('Datos recibidos:', req.body)
@@ -33,13 +35,13 @@ app.get('/get/img', async (req, res) => {
 
   const { data, error } = await supabase
     .from('images')
-    .select('*');
+    .select('*')
   if (error) return res.status(400).json({ error });
-  res.json(data);
+  res.json(data)
 
 })
 
 // Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+})
