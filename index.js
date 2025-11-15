500fe2b6299580c8f894a97fd9ed140fc868e63d
kevvj <kevinjvillalba774+1@gmail.com>
2025-11-15 02:24:16 -0500

    .

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 const PORT = 3000;
 
-// Middleware para parsear JSON
+const supabaseUrl = 'https://qukomivcjxiwtmecebnb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1a29taXZjanhpd3RtZWNlYm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNzEwOTQsImV4cCI6MjA3ODc0NzA5NH0.lIOvl8Fis8dwaGlQHvDy4qsZ1-W2NQJ_zTC0uHE_sRY';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 app.use(bodyParser.json());
 
-// Ruta de prueba para recibir datos
 app.post('/api/test', (req, res) => {
-  console.log('Datos recibidos:', req.body);
-  res.json({ status: 'ok', received: req.body });
-});
+  console.log('Datos recibidos:', req.body)
+  res.json({ status: 'ok', received: req.body })
+})
 
-// Ruta GET de prueba
 app.get('/api/test', (req, res) => {
-  res.json({ message: 'API funcionando' });
-});
+  res.json({ message: 'API funcionando' })
+})
+
+app.post('/add/img', async (req, res) => {
+  const { content } = req.body;
+  const { data, error } = await supabase
+    .from('images')
+    .insert([{content}])
+  if (error) return res.status(400).json({ error });
+  res.json(data)
+})
+
+app.get('/get/img', async (req, res) => {
+
+  const { data, error } = await supabase
+    .from('images')
+    .select('*');
+  if (error) return res.status(400).json({ error });
+  res.json(data);
+
+})
 
 // Iniciar servidor
 app.listen(PORT, () => {
